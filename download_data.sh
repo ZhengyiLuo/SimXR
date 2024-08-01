c12adc7cd94227c56b3bd8943ab2a5b05f82bd7a
Zhengyi <zhengyiluo5@gmail.com>
2024-07-31 20:46:39 -0400

    updating aria model training cdoe

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,12 @@
 mkdir sample_data
-mkdir -p output output/HumanoidIm/ output/HumanoidIm/simxr_aria
+mkdir -p output output/HumanoidIm/ output/HumanoidIm/simxr_aria output/HumanoidIm/phc_z_fut_aria_1
 gdown https://drive.google.com/uc?id=1bLp4SNIZROMB7Sxgt0Mh4-4BLOPGV9_U -O  sample_data/ # filtered shapes from AMASS
 gdown https://drive.google.com/uc?id=1arpCsue3Knqttj75Nt9Mwo32TKC4TYDx -O  sample_data/ # all shapes from AMASS
 gdown https://drive.google.com/uc?id=1fFauJE0W0nJfihUvjViq9OzmFfHo_rq0 -O  sample_data/ # sample standing neutral data.
 gdown https://drive.google.com/uc?id=1uzFkT2s_zVdnAohPWHOLFcyRDq372Fmc -O  sample_data/ # amass_occlusion_v3
 gdown https://drive.google.com/uc?id=1oQEDHzwZ3s20WbHyWfOyjcU2ho81ewHA -O  sample_data/ # aria sample data 
 gdown https://drive.google.com/uc?id=107i5YyM_2a2MkPTfJ04z-PGpREa02fVM -O  output/HumanoidIm/simxr_aria/Humanoid.pth # aria model 
+gdown https://drive.google.com/uc?id=189xUxpKQXLok93-hgNjhPvtgI6r5wkkg -O  output/HumanoidIm/phc_z_fut_aria_1/Humanoid_00017000.pth # aria model 
+
+https://drive.google.com/file/d/189xUxpKQXLok93-hgNjhPvtgI6r5wkkg/view?usp=sharing
 
